8cdbf2d0983c8f452aeef9023838a3084e498b52
CozyWool <vsergeev201530@gmail.com>
2024-02-11 21:27:15 +0500

    Сделал все команды

---
+++ OlympiadWpfApp/OlympiadWpfApp/Models/MedalTableModel.cs
@@ -0,0 +1,10 @@
+﻿namespace OlympiadWpfApp.Models;
+
+public class MedalTableModel
+{
+    public string Country { get; set; }
+    public int GoldMedals { get; set; }
+    public int SilverMedals { get; set; }
+    public int BronzeMedals { get; set; }
+    public int TotalMedals => GoldMedals + SilverMedals + BronzeMedals;
+}
--- OlympiadWpfApp/OlympiadWpfApp/ViewModels/MainWindowViewModel.cs
+++ OlympiadWpfApp/OlympiadWpfApp/ViewModels/MainWindowViewModel.cs
@@ -3,37 +3,20 @@ using System.ComponentModel;
 using System.Data;
 using System.Runtime.CompilerServices;
 using System.Windows;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using OlympiadWpfApp.Commands;
 using OlympiadWpfApp.DataAccess.Contexts;
+using OlympiadWpfApp.DataAccess.Entities;
+using OlympiadWpfApp.Extensions;
+using OlympiadWpfApp.Models;
 
 namespace OlympiadWpfApp.ViewModels;
-
-public static class ListExtensions
-{
-    public static DataTable ToDataTable<T>(this IList<T> data) // со stackoverflow взято
-    {
-        PropertyDescriptorCollection properties =
-            TypeDescriptor.GetProperties(typeof(T));
-        DataTable table = new DataTable();
-        foreach (PropertyDescriptor prop in properties)
-            table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
-        foreach (T item in data)
-        {
-            DataRow row = table.NewRow();
-            foreach (PropertyDescriptor prop in properties)
-                row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
-            table.Rows.Add(row);
-        }
-
-        return table;
-    }
-}
 
 public class MainWindowViewModel : INotifyPropertyChanged
 {
     private Window _owner;
-    private readonly string allOlympiadsString;
+    private readonly string _allOlympiadsString;
 
     private readonly OlympDbContext _dbContext;
     private DataView _queryResult;
@@ -46,13 +29,16 @@ public class MainWindowViewModel : INotifyPropertyChanged
         get
         {
             var result = new ObservableCollection<string>(_dbContext.Olympiads.Select(x => x.Name).Distinct().ToList());
-            result.Add(allOlympiadsString);
+            result.Add(_allOlympiadsString);
             return result;
         }
     }
+    public ObservableCollection<string> SportTypes =>
+        new(_dbContext.SportTypes.Select(x => x.Name).Distinct().ToList());
 
     public string? SelectedCountry { get; set; }
     public string? SelectedOlympiad { get; set; }
+    public string? SelectedSportType { get; set; }
 
     public DataView QueryResult
     {
@@ -76,13 +62,20 @@ public class MainWindowViewModel : INotifyPropertyChanged
     public MainWindowViewModel(Window owner)
     {
         _owner = owner;
-        allOlympiadsString = "Все олимпиады"; // TODO: можно через ресурсы сюда локализацию подключить
+        _allOlympiadsString = "Все олимпиады"; // TODO: можно через ресурсы сюда локализацию подключить
         ShowMedalTable = new DelegateCommand(_ => ExecuteShowMedalTable(), _ => NeedSelectedOlympiad());
+        ShowMedalists = new DelegateCommand(_ => ExecuteShowMedalists(), _ => NeedSelectedOlympiad());
+        ShowMostGoldMedalsCountry =
+            new DelegateCommand(_ => ExecuteShowMostGoldMedalsCountry(), _ => NeedSelectedOlympiad());
+        ShowMostHostCountry = new DelegateCommand(_ => ExecuteShowMostHostCountry());
+        ShowMostGoldMedalsParticipant = new DelegateCommand(_ => ExecuteShowMostGoldMedalsParticipant(), _ => NeedSelectedSportType());
+        ShowCountryTeam  = new DelegateCommand(_ => ExecuteShowCountryTeam(), _ => NeedSelectedCountry());
+        ShowCountryStats = new DelegateCommand(_ => ExecuteShowCountryStats(), _ => NeedSelectedCountry() && NeedSelectedOlympiad());
+
 
         var configuration = BuildConfiguration();
         _dbContext = new OlympDbContext(configuration);
     }
-
     private bool NeedSelectedCountry()
     {
         return SelectedCountry != null;
@@ -93,49 +86,220 @@ public class MainWindowViewModel : INotifyPropertyChanged
         return SelectedOlympiad != null;
     }
 
+    private bool NeedSelectedSportType()
+    {
+        return SelectedSportType != null;
+    }
+
     private void ExecuteShowMedalTable()
     {
-        if (SelectedOlympiad == allOlympiadsString)
+        if (SelectedOlympiad == _allOlympiadsString)
         {
-            QueryResult = _dbContext.Participants.GroupBy(p => p.Country).Join(_dbContext.SportTypeParticipants, p => p.Id,
-                sp => sp.ParticipantId,
-                (p, sp) => new
-                {
-                    Country = p.Country,
-                    GoldMedals = sp.GoldMedals,
-                    SilverMedals = sp.SilverMedals,
-                    BronzeMedals = sp.BronzeMedals
-                }).ToList().ToDataTable().DefaultView;
+            var query = from p in _dbContext.Participants
+                join sp in _dbContext.SportTypeParticipants on p.Id equals sp.ParticipantId
+                select new {p, sp};
+            QueryResult = (from q in query
+                    group q by q.p.Country
+                    into gr
+                    select new MedalTableModel
+                    {
+                        Country = gr.Key,
+                        GoldMedals = gr.Sum(x => x.sp.GoldMedals),
+                        SilverMedals = gr.Sum(x => x.sp.SilverMedals),
+                        BronzeMedals = gr.Sum(x => x.sp.BronzeMedals)
+                    })
+                .OrderByDescending(x => x.GoldMedals)
+                .ToList().ToDataTable().DefaultView;
             return;
         }
 
-        QueryResult = _dbContext.Participants
-            .Join(_dbContext.OlympiadParticipants, p => p.Id, op => op.ParticipantId,
-                (p, op) => new
+        var query1 = from p in _dbContext.Participants
+                .Include(x => x.OlympiadParticipant)
+                .ThenInclude(x => x.OlympiadEntity)
+            join sp in _dbContext.SportTypeParticipants on p.Id equals sp.ParticipantId
+            where p.OlympiadParticipant.OlympiadEntity.Name == SelectedOlympiad
+            select new {p, sp};
+        QueryResult = (from q in query1
+                group q by q.p.Country
+                into gr
+                select new MedalTableModel
                 {
-                    Participant = p, OlympName = op.OlympiadEntity.Name
+                    Country = gr.Key,
+                    GoldMedals = gr.Sum(x => x.sp.GoldMedals),
+                    SilverMedals = gr.Sum(x => x.sp.SilverMedals),
+                    BronzeMedals = gr.Sum(x => x.sp.BronzeMedals)
                 })
-            .Where(x => x.OlympName == SelectedOlympiad).Join(_dbContext.SportTypeParticipants, p => p.Participant.Id,
-                sp => sp.ParticipantId,
-                (p, sp) => new
-                {
-                    Country = p.Participant.Country,
-                    GoldMedals = sp.GoldMedals,
-                    SilverMedals = sp.SilverMedals,
-                    BronzeMedals = sp.BronzeMedals
-                })
-            .ToList()
-            .ToDataTable()
-            .DefaultView;
+            .OrderByDescending(x => x.GoldMedals)
+            .ToList().ToDataTable().DefaultView;
     }
 
     private void ExecuteShowMedalists()
     {
-        if (SelectedOlympiad == allOlympiadsString)
+        if (SelectedOlympiad == _allOlympiadsString)
         {
+            QueryResult = (from p in _dbContext.Participants
+                    join sp in _dbContext.SportTypeParticipants on p.Id equals sp.ParticipantId
+                    select new
+                    {
+                        p.Country,
+                        SportType = sp.SportTypeEntity.Name,
+                        p.Surname,
+                        p.Name,
+                        p.Patronymic,
+                        sp.GoldMedals,
+                        sp.SilverMedals,
+                        sp.BronzeMedals
+                    })
+                .OrderByDescending(x => x.GoldMedals)
+                .ToList().ToDataTable().DefaultView;
+            return;
         }
+
+        QueryResult = (from p in _dbContext.Participants
+                    .Include(x => x.OlympiadParticipant)
+                    .ThenInclude(x => x.OlympiadEntity)
+                join sp in _dbContext.SportTypeParticipants on p.Id equals sp.ParticipantId
+                where p.OlympiadParticipant.OlympiadEntity.Name == SelectedOlympiad
+                select new
+                {
+                    p.Country,
+                    SportType = sp.SportTypeEntity.Name,
+                    p.Surname,
+                    p.Name,
+                    p.Patronymic,
+                    sp.GoldMedals,
+                    sp.SilverMedals,
+                    sp.BronzeMedals
+                })
+            .OrderByDescending(x => x.GoldMedals)
+            .ToList().ToDataTable().DefaultView;
     }
 
+    private void ExecuteShowMostGoldMedalsCountry()
+    {
+        if (SelectedOlympiad == _allOlympiadsString)
+        {
+            var query = from p in _dbContext.Participants
+                join sp in _dbContext.SportTypeParticipants on p.Id equals sp.ParticipantId
+                select new {p, sp};
+            QueryResult = (from q in query
+                    group q by q.p.Country
+                    into gr
+                    select new
+                    {
+                        Country = gr.Key,
+                        GoldMedals = gr.Sum(x => x.sp.GoldMedals)
+                    })
+                .OrderByDescending(x => x.GoldMedals).Take(1)
+                .ToList().ToDataTable().DefaultView;
+            return;
+        }
+
+        var query1 = from p in _dbContext.Participants
+                .Include(x => x.OlympiadParticipant)
+                .ThenInclude(x => x.OlympiadEntity)
+            join sp in _dbContext.SportTypeParticipants on p.Id equals sp.ParticipantId
+            where p.OlympiadParticipant.OlympiadEntity.Name == SelectedOlympiad
+            select new {p, sp};
+        QueryResult = (from q in query1
+                group q by q.p.Country
+                into gr
+                select new
+                {
+                    Country = gr.Key,
+                    GoldMedals = gr.Sum(x => x.sp.GoldMedals)
+                })
+            .OrderByDescending(x => x.GoldMedals).Take(1)
+            .ToList().ToDataTable().DefaultView;
+    }
+
+    private void ExecuteShowMostGoldMedalsParticipant()
+    {
+        QueryResult = _dbContext.Participants.Join(_dbContext.SportTypeParticipants, p => p.Id, sp => sp.ParticipantId,
+                (p, sp) => new
+                {
+                    p.Country,
+                    SportType = sp.SportTypeEntity.Name,
+                    p.Surname,
+                    p.Name,
+                    p.Patronymic,
+                    sp.GoldMedals,
+                    sp.SilverMedals,
+                    sp.BronzeMedals
+                }).Where(x => x.SportType == SelectedSportType)
+            .OrderByDescending(x => x.GoldMedals).Take(1)
+            .ToList().ToDataTable().DefaultView;
+    }
+
+    private void ExecuteShowMostHostCountry()
+    {
+        QueryResult = _dbContext.Olympiads.GroupBy(o => o.HostCountry, o => o, (country, count) => new
+            {
+                Country = country,
+                Count = count.Count()
+            })
+            .OrderByDescending(x => x.Count).Take(1)
+            .ToList().ToDataTable().DefaultView;
+    }
+
+    private void ExecuteShowCountryTeam()
+    {
+        QueryResult = _dbContext.Participants
+            .Where(x => x.Country == SelectedCountry).Distinct()
+            .Select(x => new
+            {
+                x.Id,
+                x.Country,
+                x.Surname,
+                x.Name,
+                x.Patronymic,
+                x.Birthdate
+            })
+            .ToList().ToDataTable().DefaultView;
+    }
+    private void ExecuteShowCountryStats()
+    {
+        if (SelectedOlympiad == _allOlympiadsString)
+        {
+            var query = from p in _dbContext.Participants
+                join sp in _dbContext.SportTypeParticipants on p.Id equals sp.ParticipantId
+                where p.Country == SelectedCountry
+                select new {p, sp};
+            QueryResult = (from q in query
+                    group q by q.p.Country
+                    into gr
+                    select new MedalTableModel
+                    {
+                        Country = gr.Key,
+                        GoldMedals = gr.Sum(x => x.sp.GoldMedals),
+                        SilverMedals = gr.Sum(x => x.sp.SilverMedals),
+                        BronzeMedals = gr.Sum(x => x.sp.BronzeMedals)
+                    })
+                .OrderByDescending(x => x.GoldMedals)
+                .ToList().ToDataTable().DefaultView;
+            return;
+        }
+
+        var query1 = from p in _dbContext.Participants
+                .Include(x => x.OlympiadParticipant)
+                .ThenInclude(x => x.OlympiadEntity)
+            join sp in _dbContext.SportTypeParticipants on p.Id equals sp.ParticipantId
+            where p.OlympiadParticipant.OlympiadEntity.Name == SelectedOlympiad && p.Country == SelectedCountry
+            select new {p, sp};
+        QueryResult = (from q in query1
+                group q by q.p.Country
+                into gr
+                select new MedalTableModel
+                {
+                    Country = gr.Key,
+                    GoldMedals = gr.Sum(x => x.sp.GoldMedals),
+                    SilverMedals = gr.Sum(x => x.sp.SilverMedals),
+                    BronzeMedals = gr.Sum(x => x.sp.BronzeMedals)
+                })
+            .OrderByDescending(x => x.GoldMedals)
+            .ToList().ToDataTable().DefaultView;
+    }
+    
     private IConfiguration BuildConfiguration()
     {
         return new ConfigurationBuilder()
